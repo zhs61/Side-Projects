cc37eb87c81625bb3d1ab4d64a8d252d78d48936
Zhongxuan Song <zhs61@pitt.edu>
2020-09-14 02:13:26 -0400

    added a expanable list

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Header from "./Header/Header.js";
+import ThoughtTracker from "./Thought-tracker/Thought_tracker";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <div className="ThoughtTracker">
+        <ThoughtTracker />
+      </div>
     </div>
   );
 }
---
+++ src/Thought-tracker/Thought_tracker.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import ListSubheader from "@material-ui/core/ListSubheader";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import Collapse from "@material-ui/core/Collapse";
+import ExpandLess from "@material-ui/icons/ExpandLess";
+import ExpandMore from "@material-ui/icons/ExpandMore";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: "100%",
+    maxWidth: 800,
+    backgroundColor: theme.palette.background.paper,
+  },
+  nested: {
+    paddingLeft: theme.spacing(4),
+  },
+}));
+
+export default function Thought_tracker() {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+
+  const handleClick = () => {
+    setOpen(!open);
+  };
+
+  return (
+    <List
+      component="nav"
+      aria-labelledby="nested-list-subheader"
+      subheader={
+        <ListSubheader component="div" id="nested-list-subheader">
+          Thought Tracker
+        </ListSubheader>
+      }
+      className={classes.root}
+    >
+      <ListItem button onClick={handleClick}>
+        <ListItemText primary="Summary" />
+        {open ? <ExpandLess /> : <ExpandMore />}
+      </ListItem>
+      <Collapse in={open} timeout="auto" unmountOnExit>
+        <List component="div" disablePadding>
+          <ListItem button className={classes.nested}>
+            <ListItemText primary="Detail" />
+          </ListItem>
+        </List>
+      </Collapse>
+    </List>
+  );
+}
